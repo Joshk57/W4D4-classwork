c3d08a6cff1466e85580ffb6aec0eb36ea85cc1c
carveyh <carvey.hor@gmail.com>
2023-03-30 16:38:21 -0400

    Add #third_anagram? and #fourth_anagram?

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -1,6 +1,9 @@
 require 'byebug'
 def first_anagram?(str1, str2)
 	anagrams = str1.split("").permutation.to_a.map { |permutation| permutation.join("") }
+	#O(1) + O(n) + O(n!) + O(n ^ 2) + O(1) 
+	#O(1 + n + n! + n^2 + 1)
+	#O(n!)
 	anagrams.include?(str2)
 end
 
@@ -8,23 +11,64 @@ end
 p first_anagram?("gizmo","sally")
 p first_anagram?("elivs","lives")
 
-
+# O(1) + O(n) + O(1) + O(n) + (n^2 + n + 4) + O(1)
+# O(n^2)
 def second_anagram?(str1, str2)
-	arr1 = str1.split("")
-	arr2 = str2.split("")
-	arr1.each_with_index do |char, idx1|
-		idx2 = arr2.index(char)
+	arr1 = str1.split("") # O(1) + O(n)
+	arr2 = str2.split("") # O(1) + O(n)
+	arr1.each_with_index do |char, idx1| # O(n)
+		idx2 = arr2.index(char) # O(1) + O(n)
 		# debugger
-		(idx2.nil?) ? (return false) : arr2.delete_at(idx2)
-		# if idx2.nil?
-		# 	return false
-		# else
-		# 	arr2.delete_at(idx2)
-		# end
+		(idx2.nil?) ? (return false) : arr2.delete_at(idx2) # O(1) + O(1) + O(1)
 	end
-	arr2.empty?
+	arr2.empty? # O(1)
 end
 
 p second_anagram?("gizmo","sally")
 p second_anagram?("elivs","lives")
 p second_anagram?("elivs","livest")
+
+#O(4(n + 1)) => O(4n + 4) => O(n) 
+def third_anagram?(str1, str2)
+	arr1 = str1.split("") # O(1 + n)
+	arr2 = str2.split("") # O(1 + n)
+	arr1.sort == arr2.sort # O(2(n + 1))
+end
+
+puts
+puts
+
+p third_anagram?("gizmo","sally")
+p third_anagram?("elivs","lives")
+p third_anagram?("elivs","livest")
+
+# def fourth_anagram?(str1, str2)
+# 	letters(str1) == letters(str2)
+# end
+
+# def letters(str)
+# 	hash = Hash.new(0)
+# 	str.each_char do |char|
+# 		hash[char] += 1
+# 	end
+# 	hash
+# end
+
+# O(4n + 1) => # O(n)
+def fourth_anagram?(str1, str2)
+	hash = Hash.new(0) #constant
+	str1.each_char do |char| # O(n)
+		hash[char] += 1 # 2 const
+	end
+	str2.each_char do |char| # O(n)
+		hash[char] -= 1 # 2 const
+	end
+	hash.values.all? { |v| v==0 } # O(n) + O(n)
+end
+
+puts
+puts
+
+p fourth_anagram?("gizmo","sally")
+p fourth_anagram?("elivs","lives")
+p fourth_anagram?("elivs","livest")
