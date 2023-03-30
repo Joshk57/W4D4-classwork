afd5aee7e06ddd6792679fe93de321c6f640513e
carveyh <carvey.hor@gmail.com>
2023-03-30 17:22:12 -0400

    Updated Big-O time calculations

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -1,10 +1,10 @@
 require 'byebug'
 def first_anagram?(str1, str2)
 	anagrams = str1.split("").permutation.to_a.map { |permutation| permutation.join("") }
-	#O(1) + O(n) + O(n!) + O(n ^ 2) + O(1) 
+	#O(1) + O(n) + O(n!) + O(n ^ 2) 
 	#O(1 + n + n! + n^2 + 1)
 	#O(n!)
-	anagrams.include?(str2)
+	anagrams.include?(str2) # O(n)
 end
 
 
@@ -13,13 +13,15 @@ p first_anagram?("elivs","lives")
 
 # O(1) + O(n) + O(1) + O(n) + (n^2 + n + 4) + O(1)
 # O(n^2)
+
+#(2n + 1) + ( n * (2n + 3))
 def second_anagram?(str1, str2)
-	arr1 = str1.split("") # O(1) + O(n)
+	arr1 = str1.split("") # O(1) + O(n) (2n +1)
 	arr2 = str2.split("") # O(1) + O(n)
-	arr1.each_with_index do |char, idx1| # O(n)
+	arr1.each_with_index do |char, idx1| # O(n) * (2n +3)
 		idx2 = arr2.index(char) # O(1) + O(n)
 		# debugger
-		(idx2.nil?) ? (return false) : arr2.delete_at(idx2) # O(1) + O(1) + O(1)
+		(idx2.nil?) ? (return false) : arr2.delete_at(idx2) # O(1) + O(1) + O(n)
 	end
 	arr2.empty? # O(1)
 end
@@ -28,11 +30,11 @@ p second_anagram?("gizmo","sally")
 p second_anagram?("elivs","lives")
 p second_anagram?("elivs","livest")
 
-#O(4(n + 1)) => O(4n + 4) => O(n) 
+# nlogn
 def third_anagram?(str1, str2)
 	arr1 = str1.split("") # O(1 + n)
 	arr2 = str2.split("") # O(1 + n)
-	arr1.sort == arr2.sort # O(2(n + 1))
+	arr1.sort == arr2.sort # O(2nlogn)
 end
 
 puts
@@ -54,7 +56,7 @@ p third_anagram?("elivs","livest")
 # 	hash
 # end
 
-# O(4n + 1) => # O(n)
+# O(7n) => # O(n)
 def fourth_anagram?(str1, str2)
 	hash = Hash.new(0) #constant
 	str1.each_char do |char| # O(n)
@@ -63,7 +65,7 @@ def fourth_anagram?(str1, str2)
 	str2.each_char do |char| # O(n)
 		hash[char] -= 1 # 2 const
 	end
-	hash.values.all? { |v| v==0 } # O(n) + O(n)
+	hash.values.all? { |v| v==0 } # O(n) + O(2n)
 end
 
 puts
