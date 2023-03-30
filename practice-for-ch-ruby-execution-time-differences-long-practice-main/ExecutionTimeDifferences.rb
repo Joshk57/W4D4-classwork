5dc6c99646f56646cc85fe5c27d3eac742319445
carveyh <carvey.hor@gmail.com>
2023-03-30 12:06:36 -0400

    Phase 2 of #my_min

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/ExecutionTimeDifferences.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/ExecutionTimeDifferences.rb
@@ -2,20 +2,32 @@
 # O(3n * 2n)
 # O(6n^2)
 # O(n^2)
+# def my_min(list)
+#     list.each do |ele1| # O(n)
+#         smallest = true # constant
+#         list.each do |ele2| # O(n)
+#             smallest = false if ele2 < ele1 #constant + constant
+#         end
+#         return ele1 if smallest # constant + constant
+#     end
+# end
+
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)  # =>  -5
+
+# O(1) + O(3n) + O(1)
+# O(1 + 3n + 1)
+# O(n)
 def my_min(list)
-    list.each do |ele1| # O(n)
-        smallest = true # constant
-        list.each do |ele2| # O(n)
-            smallest = false if ele2 < ele1 #constant + constant
+    num = list.first # constant O(1)
+    list.each_with_index do |ele, i| # O(n)
+        # if i > 0 && num > ele # constant + constant # i > 0 is not necessary
+        if num > ele # constant + constant
+            num = ele # constant
         end
-        return ele1 if smallest # constant + constant
-    end
+    end    
+    return num # constant
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min(list)  # =>  -5
-
-def my_min(list)
-
-
-end
